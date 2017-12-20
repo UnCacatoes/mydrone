129df2eb55a39512c9050ebd65519253e48dedd4
UnCacatoes <guillaume.gresillion@gmail.com>
2017-12-20 13:22:10 +0100

    New settings menu

--- app/src/androidTest/java/fr/telecomlille/mydrone/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/fr/telecomlille/mydrone/ExampleInstrumentedTest.java
@@ -1,13 +1,21 @@
 package fr.telecomlille.mydrone;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import fr.telecomlille.mydrone.recognition.RecognitionActivity;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,6 +24,20 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<RecognitionActivity> mActivityRule = new ActivityTestRule<RecognitionActivity>(RecognitionActivity.class) {
+        @Override
+        protected Intent getActivityIntent() {
+            Context targetContext = InstrumentationRegistry.getInstrumentation()
+                    .getTargetContext();
+            Intent result = new Intent(targetContext, MainActivity.class);
+            ARDiscoveryDeviceService mockService = mock(ARDiscoveryDeviceService.class);
+            result.putExtra("DeviceService", mockService);
+            return result;
+        }
+    };;
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
